5090b3390c89cd27386269e24f1b54d361fed9d8
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-14 09:04:40 +0530

    v1.0

--- downloadFiles.py
+++ downloadFiles.py
@@ -9,7 +9,7 @@ res.raise_for_status()  # simple check for success, raises exception if download
 
 playFile = open("RomeoAndJuliet.txt", "wb")  # wb: to maintain unicode
 
-# For converting website to file
-# for chunk in res.iter_content(100000):
-#     playFile.write(chunk)
-# playFile.close()
+# For converting website page to file
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+playFile.close()
